c9af05ed24fbbda074b07329a53701ecd51ff1a8
collins muriuki <muriukicollins48@gmail.com>
2025-05-19 11:05:03 +0300

    initial commit

---
+++ tests/order_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestOrder(unittest.TestCase):
+    def test_order_creation(self):
+        # TODO: implement test for order creation
+        pass
+
+    def test_order_total(self):
+        # TODO: implement test for calculating order total
+        pass
+
+    def test_order_add_item(self):
+        # TODO: implement test for adding item to order
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
